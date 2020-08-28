83cc9246dabacfe3e6e95a40e84f5bff6ef04b66
HemaPartha <hema.parthasarathy@avalara.com>
2020-08-28 14:52:27 +0530

    AkkaHttp - HelloService

---
+++ AkkaHttp/src/main/scala/HelloService.scala
@@ -0,0 +1,52 @@
+
+package com.akkahttp
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.{Directives, Route}
+import akka.stream.ActorMaterializer
+
+import spray.json.DefaultJsonProtocol
+
+
+case class Greetings(message: String)
+
+case class Status(status: Int)
+
+trait Jsonconvert extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val greetFormat = jsonFormat1(Greetings)
+  implicit val statusFormat = jsonFormat1(Status)
+}
+
+class AkkaHttpService extends Directives with Jsonconvert {
+
+  def routes(): Route = {
+    path("greet" / Segment) { name =>
+      get {
+        complete(
+          Greetings("Hello, " + name))
+      }
+    } ~
+      path("health") {
+        complete {
+          Status(StatusCodes.OK.intValue)
+        }
+      }
+  }
+}
+
+object HttpService {
+  def main(args: Array[String]): Unit = {
+    implicit val actorSystem = ActorSystem("system")
+    implicit val actorMaterializer = ActorMaterializer()
+
+    val route = new AkkaHttpService().routes()
+
+    Http().newServerAt("localhost", 1234).bind(route)
+
+    println("server started at 8080")
+  }
+
+}
