ccd9dab3a15e4e3da5f3313c8ec4ed085fee2fc6
HemaPartha <hema.parthasarathy@avalara.com>
2020-09-07 21:43:29 +0530

    added docker username and password

--- scripts/PublishDocker.sh
+++ scripts/PublishDocker.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo Publishing docker image........
 
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker build -t bootcamp .
 docker images
 docker tag bootcamp hemaparthasarathy/bootcamp
