2636d446878fcb092173f5fddbaa0cf672b282f7
HemaPartha <hema.parthasarathy@avalara.com>
2020-09-07 22:04:38 +0530

    print usrname

--- scripts/PublishDocker.sh
+++ scripts/PublishDocker.sh
@@ -2,6 +2,7 @@
 echo Publishing docker image........
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+echo $DOCKER_USERNAME
 docker build -t bootcamp .
 docker images
 docker tag bootcamp hemaparthasarathy/bootcamp
