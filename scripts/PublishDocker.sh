8a45d46f6b2d8c75a86b5bceef69f5aa3fc4583f
HemaPartha <hema.parthasarathy@avalara.com>
2020-09-07 19:19:14 +0530

    reverting script changes

--- scripts/PublishDocker.sh
+++ scripts/PublishDocker.sh
@@ -4,7 +4,7 @@ sbt clean compile
 BUILD_NUMBER=1
 echo Publishing docker image........
 
-docker build -t bootcamp-wk4 .
+docker build -t bootcamp .
 docker images
-docker tag bootcamp-wk4 hemaparthasarathy/bootcamp-wk4
-docker push hemaparthasarathy/bootcamp-wk4
+docker tag bootcamp-wk4 hemaparthasarathy/bootcamp
+docker push hemaparthasarathy/bootcamp
