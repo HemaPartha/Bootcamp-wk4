47131193a1fff47af4871bf26fa356608f46a927
HemaPartha <hema.parthasarathy@avalara.com>
2020-09-07 23:15:37 +0530

    append build number to docker version

--- scripts/PublishDocker.sh
+++ scripts/PublishDocker.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 echo Publishing docker image........
 echo "build" + $TRAVIS_BUILD_NUMBER
+echo $PACKAGE_NAME
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-echo $DOCKER_USERNAME
+
 docker build -t bootcamp .
 docker images
 docker tag bootcamp hemaparthasarathy/bootcamp
-docker push hemaparthasarathy/bootcamp
+docker push hemaparthasarathy/bootcamp:$TRAVIS_BUILD_NUMBER
