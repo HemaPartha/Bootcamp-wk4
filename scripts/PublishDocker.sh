8340ff52864b61b672e0fc45367456b152638962
HemaPartha <hema.parthasarathy@avalara.com>
2020-09-07 20:38:22 +0530

    fixeed wrong imagename

--- scripts/PublishDocker.sh
+++ scripts/PublishDocker.sh
@@ -6,5 +6,5 @@ echo Publishing docker image........
 
 docker build -t bootcamp .
 docker images
-docker tag bootcamp-wk4 hemaparthasarathy/bootcamp
+docker tag bootcamp hemaparthasarathy/bootcamp
 docker push hemaparthasarathy/bootcamp
