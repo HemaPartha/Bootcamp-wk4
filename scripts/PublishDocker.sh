36a48c378b5f0fd6a315e0987387a5cd3bc8c90b
HemaPartha <hema.parthasarathy@avalara.com>
2020-09-07 18:08:09 +0530

    add build to dockerpublish

--- scripts/PublishDocker.sh
+++ scripts/PublishDocker.sh
@@ -1,8 +1,10 @@
-BUILD_NUMBER=$1
-echo Publishing docker image.........
+#!/usr/bin/env bash
+sbt clean compile
 
+BUILD_NUMBER=1
+echo Publishing docker image........
 
 docker build -t bootcamp-wk4 .
 docker images
-docker tag bootcamp-wk4 hemaparthasarathy/bootcamp
-docker push hemaparthasarathy/bootcamp
+docker tag bootcamp-wk4 hemaparthasarathy/bootcamp-wk4
+docker push hemaparthasarathy/bootcamp-wk4
